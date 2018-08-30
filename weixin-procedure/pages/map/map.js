3b8683256b353dd9e59bbd5ca7bb8757f71d5780
18330697972 <18330697972@163.com>
2018-08-30 11:38:31 +0800

    map

---
+++ weixin-procedure/pages/map/map.js
@@ -0,0 +1,86 @@
+// pages/map/map.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // wx.getLocation({
+    //   type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+    //   success: function (res) {
+    //     var latitude = res.latitude
+    //     var longitude = res.longitude
+    //     wx.openLocation({
+    //       latitude: latitude,
+    //       longitude: longitude,
+    //       scale: 28
+    //     })
+    //   }
+    // })
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    let coordinate = "116.501375,39.980545";
+    let address = "北京市朝阳区酒仙桥路14号院C区-3号楼";
+
+    wx.openLocation({
+      latitude: parseFloat(22.5542080000),//要去的纬度-地址
+      longitude: parseFloat(113.8878770000),//要去的经度-地址
+      name: "北京市朝阳区酒仙桥路14号院C区-3号楼",
+      address: "北京市朝阳区酒仙桥路14号院C区-3号楼"
+    })  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
